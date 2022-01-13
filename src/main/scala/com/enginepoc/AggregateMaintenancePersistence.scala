d433667f25fb3a6f35f8ec63b712bbf4e7025f4a
Plamen5kov <plamen.krasimirov.petkov@gmail.com>
2022-01-13 14:30:37 +0200

    clean up

--- src/main/scala/com/enginepoc/AggregateMaintenancePersistence.scala
+++ src/main/scala/com/enginepoc/AggregateMaintenancePersistence.scala
@@ -7,15 +7,15 @@ import scala.concurrent.ExecutionContext
 
 object AggregateMaintenancePersistence {
 
-  trait Message
-  case class Hello(name: String) extends Message
-
   def apply(): Behavior[Message] = Behaviors.setup[Message] { context =>
     implicit val ec: ExecutionContext = context.executionContext
 
-    Behaviors.receiveMessage {
-      case message: Message => context.log.debug(s"message: $message received")
-        Behaviors.same
+    Behaviors.receiveMessage { case message: Message => context.log.debug(s"message: $message received")
+      Behaviors.same
     }
   }
+
+  trait Message
+
+  case class Hello(name: String) extends Message
 }
--- src/main/scala/com/enginepoc/AggregateSystem.scala
+++ src/main/scala/com/enginepoc/AggregateSystem.scala
@@ -2,8 +2,6 @@ package com.enginepoc
 
 import akka.actor
 import akka.actor.{ActorSystem, Props}
-import akka.actor.typed.scaladsl.Behaviors
-import akka.actor.typed.{ActorRef, Behavior}
 
 object AggregateSystem extends App {
   val system: ActorSystem = ActorSystem("aggregateSystem")
